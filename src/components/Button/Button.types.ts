2810a578bf0c28908460d3bfb3e8884f34637582
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-01-08 19:36:30 -0300

    Fixing button props

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -3,11 +3,11 @@ import BasicProps from "../../types/basics";
 
 export default interface ComponentProps extends BasicProps {
     children?: any,
-    variant: 'filled' | 'outlined' | 'filled-outlined' | 'outlined-filled',
-    size: number,
+    variant?: 'filled' | 'outlined' | 'filled-outlined' | 'outlined-filled',
+    size?: number,
     onClick?: MouseEventHandler<HTMLButtonElement>,
     disabled?: boolean,
     rounded?: boolean,
     label?: string,
-    type: 'button' | 'reset' | 'submit'
+    type?: 'button' | 'reset' | 'submit'
 }
